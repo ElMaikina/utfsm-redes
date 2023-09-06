611252c6a2b6314366608006c572f7778945b925
ElMaikina <mikesoto890@gmail.com>
2023-09-06 13:49:42 -0300

    added greeter and refactored

--- client.py
+++ client.py
@@ -6,37 +6,55 @@ IP = "localhost"
 PORT = 8000
 
 # Crea un socket para conectarse al servidor intermediario
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect((IP, PORT))
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server_socket.connect((IP, PORT))
 
+# Variable que mantiene el juego funcionando
 game_is_running = True
 
-def run():
-    new_msg_in = True
-    full_msg_in = ""
-    msg_in_len = 0
+# Recibe el mensaje del servidor
+def listen_to_server():
+    new_message_in = True # Llego carta?
+    full_message_in = ""  # Mensaje completo
+    message_in_len = 0    # Largo del mensaje
 
     while True:
-        msg_in = s.recv(16)
+        message_in = server_socket.recv(16)
+        #print(f"Nuevo mensaje sin decodificar: {message_in}")
 
-        if msg_in[:HEADERSIZE] == b'':
+        # Si el mensaje es vacio, avisar al usuario y salir
+        if message_in[:HEADERSIZE] == b'':
             print("No hay respuesta...")
             return
 
-        if new_msg_in:
-            msg_in_len = int(msg_in[:HEADERSIZE])
-            new_msg_in = False
-    
-        full_msg_in += msg_in.decode("utf-8")
-    
-        if len(full_msg_in)-HEADERSIZE == msg_in_len:
-            print(full_msg_in[HEADERSIZE:])
-            new_msg_in = True
-            full_msg_in = ""
+        # Si llego un mensaje, buscar el largo
+        if new_message_in:
+            message_in_len = int(message_in[:HEADERSIZE])
+            new_message_in = False
+   
+        # Decodificar el mensaje en formato UTF-8
+        full_message_in += message_in.decode("utf-8")
+   
+        # Si el mensaje leido es igual a la ultima porcion
+        # del mensaje total, mostrarlo por pantalla y salir
+        # de la funcion
+        if len(full_message_in)-HEADERSIZE == message_in_len:
+            print(full_message_in[HEADERSIZE:])
+            new_message_in = True
+            full_message_in = ""
             return
 
+# Analiza la respuesta del jugador humano
+def analyze_play(play):
+    if play == "Salir":
+        print("Saliendo del juego")
+        game_is_ruinning = False
+        exit()
+
+# Funcion main
 if __name__ == '__main__':
     while game_is_running:
-        run()
+        listen_to_server()
         play = input("->")
+        analyze_play(play)
 
--- server.py
+++ server.py
@@ -11,21 +11,35 @@ server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 
 # Se conecta y recibe informacion
 server_socket.bind((IP, PORT))
-server_socket.listen(1)
+server_socket.listen(2)
 client_socket, address = server_socket.accept()
 
+# Variable que mantiene el juego funcionando
 game_is_running = True
 
-def run():
-    msg_out = ""
-    msg_out += "\nBienvenido al Juego\n"
-    msg_out += "Seleccione una opcion\n"
-    msg_out += "1-Jugar\n"
-    msg_out += "2-Salir"
-    msg_out = f"{len(msg_out):<{HEADERSIZE}}" + msg_out
-    client_socket.send(bytes(msg_out, "utf-8"))
-    print("Message sent!")
+# Enviarle el mensaje de bienvenida al jugador
+def greet_the_client():
+    message = ""
+    message += "\nBienvenido al Juego\n"
+    message += "Seleccione una opcion\n"
+    message += "1-Jugar\n"
+    message += "2-Salir"
+    message = f"{len(message):<{HEADERSIZE}}" + message
+    client_socket.send(bytes(message, "utf-8"))
+    print("Server: message sent")
+    return
+
+# Enviarle un mensaje al cliente jugador
+def talk_to_client():
+    message = "Elija donde poner la pieza"
+    message = f"{len(message):<{HEADERSIZE}}" + message
+    client_socket.send(bytes(message, "utf-8"))
+    print("Server: message sent")
+    return
 
 if __name__ == '__main__':
-    run()
+    greet_the_client()
+    # Convertirlo en un loop y recibir mensajes del cliente
+    #while game_is_running:
+    talk_to_client()
 

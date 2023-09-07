8834cf48109e43ec85e92dcced4ba75b28366166
JoaquinContreras1114 <contrerasjoaquin1114@gmail.com>
2023-09-06 23:26:07 -0300

    Oliver

--- client.py
+++ client.py
@@ -11,7 +11,27 @@ server_socket.connect((IP, PORT))
 
 # Variable que mantiene el juego funcionando
 game_is_running = True
-
+def CambiarTablero(Tablero, col, player):
+    # Find the first available row in the specified column
+    for row in range(len(Tablero) - 1, -1, -1):
+        if Tablero[row][col] == 0:
+            Tablero[row][col] = player
+            return Tablero
+    return None
+def PrintTablero(tablero):
+    PrintTable=[["","","","","",""],["","","","","",""],["","","","","",""],["","","","","",""],["","","","","",""],["","","","","",""],["","","","","",""]]
+    print(".  0 | 1 | 2 | 3 | 4 | 5")
+    for x in range(6):
+        for y in range(6):
+            if tablero[x][y]==0:
+                PrintTable[x][y]="\u26AB"
+            elif tablero[x][y]==1:
+                PrintTable[x][y]="\U0001F7E2"
+            else:
+                PrintTable[x][y]="\U0001F7E0"
+    for x in range(6):
+        string="{} {}  {}  {}  {}  {}  {}"
+        print(string.format(x,PrintTable[x][0],PrintTable[x][1],PrintTable[x][2],PrintTable[x][3],PrintTable[x][4],PrintTable[x][5]))
 # Recibe el mensaje del servidor
 def listen_to_server():
     new_message_in = True # Llego carta?
@@ -45,16 +65,40 @@ def listen_to_server():
             return
 
 # Analiza la respuesta del jugador humano
-def analyze_play(play):
-    if play == "Salir":
-        print("Saliendo del juego")
-        game_is_ruinning = False
-        exit()
 
 # Funcion main
 if __name__ == '__main__':
     while game_is_running:
-        listen_to_server()
+        print("- Seleccione una opcion\n1- Jugar\n2- Salir")
         play = input("->")
-        analyze_play(play)
+        if(play=="2"):
+            game_is_running=False
+            msg = "Finalizar"
+            server_socket.send(msg.encode())
+        elif(play=="1"):
+            print("--------Conecta4--------")
+            flag=True
+            Tablero=[[0,0,0,0,0,0],[0,0,0,0,0,0],[0,0,0,0,0,0],[0,0,0,0,0,0],[0,0,0,0,0,0],[0,0,0,0,0,0]]
+            PrintTablero(Tablero)
+            while(flag):
+                jugada = input('Ingrese la columna en la que desea jugar su ficha: ')
+                server_socket.send(jugada.encode())
+                Tablero=CambiarTablero(Tablero,int(jugada),1)
+                PrintTablero(Tablero)
+                response = server_socket.recv(1024).decode()
+                #Condiciones de Vicotria
+                if(response=="Ganaste"):
+                    flag2=False
+                    print(">>>Gano el Jugador<<<")
+                    print("==============================")
+                elif(response=="Gano CPU"):
+                    flag2=False
+                    print(">>>Gano la CPU<<<")
+                    print("==============================")
+                else:#condicion de siguiente Turno
+                    tablero=CambiarTablero(Tablero,int(response),2)
+                    PrintTablero(Tablero)
+                
+                
+
 
--- server.py
+++ server.py
@@ -11,12 +11,50 @@ server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 
 # Se conecta y recibe informacion
 server_socket.bind((IP, PORT))
-server_socket.listen(2)
+server_socket.listen(5)
 client_socket, address = server_socket.accept()
 
 # Variable que mantiene el juego funcionando
 game_is_running = True
+def CambiarTablero(Tablero, col, player):
+    # Find the first available row in the specified column
+    for row in range(len(Tablero) - 1, -1, -1):
+        if Tablero[row][col] == 0:
+            Tablero[row][col] = player
+            return Tablero
+    return None
+def PrintTablero(tablero):
+    PrintTable=[["","","","","",""],["","","","","",""],["","","","","",""],["","","","","",""],["","","","","",""],["","","","","",""],["","","","","",""]]
+    print(".  0 | 1 | 2 | 3 | 4 | 5")
+    for x in range(6):
+        for y in range(6):
+            if tablero[x][y]==0:
+                PrintTable[x][y]="\u26AB"
+            elif tablero[x][y]==1:
+                PrintTable[x][y]="\U0001F7E2"
+            else:
+                PrintTable[x][y]="\U0001F7E0"
+    for x in range(6):
+        string="{} {}  {}  {}  {}  {}  {}"
+        print(string.format(x,PrintTable[x][0],PrintTable[x][1],PrintTable[x][2],PrintTable[x][3],PrintTable[x][4],PrintTable[x][5]))
+def ComprobarEstado(tablero):
+    def check_line(row, col, dr, dc, player):
+        for _ in range(4):
+            if row < 0 or row >= len(tablero) or col < 0 or col >= len(tablero[0]) or tablero[row][col] != player:
+                return 0
+            row += dr
+            col += dc
+        return player
 
+    for player in [1, 2]:
+        for row in range(len(tablero)):
+            for col in range(len(tablero[0])):
+                for dr, dc in [(0, 1), (1, 0), (1, 1), (-1, 1)]:
+                    winner = check_line(row, col, dr, dc, player)
+                    if winner != 0:
+                        return winner
+    
+    return 0
 # Enviarle el mensaje de bienvenida al jugador
 def greet_the_client():
     message = ""
@@ -36,10 +74,69 @@ def talk_to_client():
     client_socket.send(bytes(message, "utf-8"))
     print("Server: message sent")
     return
-
+#
 if __name__ == '__main__':
-    greet_the_client()
-    # Convertirlo en un loop y recibir mensajes del cliente
-    #while game_is_running:
-    talk_to_client()
+    try:
+        serverPort = 8001
+        IP = 'localhost'
+        conecta4_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    except:
+        print("Error: No se pudo conectar con servidor conecta4")
+        game_is_running=False
+    if(game_is_running):
+        print("Conexiones Verificadas, Iniciando Conecta4")
+        Tablero=[[0,0,0,0,0,0],[0,0,0,0,0,0],[0,0,0,0,0,0],[0,0,0,0,0,0],[0,0,0,0,0,0],[0,0,0,0,0,0]]
+        PrintTablero(Tablero)
+    while(game_is_running):
+        PlayerMessage = client_socket.recv(1024).decode()
+        if(PlayerMessage=="Salir"):
+            print("Saliendo del juego")
+            msg="Salir"
+            conecta4_socket.send(msg.encode())
+            game_is_ruinning = False
+            exit()
+        else:
+            print("Jugada Player-> Columna ({})".format(PlayerMessage))
+            Tablero=CambiarTablero(Tablero,int(PlayerMessage[0]),1)
+            PrintTablero(Tablero)
+            if(ComprobarEstado(Tablero)==1):
+                Tablero=[[0,0,0,0,0,0],[0,0,0,0,0,0],[0,0,0,0,0,0],[0,0,0,0,0,0],[0,0,0,0,0,0],[0,0,0,0,0,0]]#Reseteo del tablero por si se quiere un siguiente Juego
+                msg="Ganaste"
+                client_socket.send(msg.encode())#Enviar al Cliente la victoria del Jugador
+                conecta4_socket.sendto("Reiniciar".encode(), (IP, serverPort))#Enviar al server el reseteo del juego para prepararse
+                print(">>>Gano el Jugador<<<")
+                print("==============================")
+            else:
+                msg=PlayerMessage[0]
+                conecta4_socket.sendto(str(msg).encode(), (IP, serverPort))
+                try:
+                    serverPort2, _ = conecta4_socket.recvfrom(1024)#Recibir puerto desde el server por UDP1
+                    serverPort2=int(serverPort2.decode())
+                    print("Recibido Puerto SOCKET UDP2 :{}".format(serverPort2))
+                    serverSocket2 = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+                    serverSocket2.bind(("",serverPort2))
+                    print("Conectado Puerto SOCKET UDP2 :{}".format(serverPort2))
+                except:
+                    pass
+                serverSocket2.sendto(str(PlayerMessage).encode(), (IP, serverPort2))
+                Mensaje, addr2 = serverSocket2.recvfrom(1025)#Jugada desde el Server UDP2
+                serverSocket2.close()
+                JugadaCPU=Mensaje.decode()
+                print(JugadaCPU)
+                print("recibida Jugada desde SOCKET UDP2 {}".format(JugadaCPU))
+                print("Jugada CPU-> Columna ({})".format(JugadaCPU))
+                print("==============================")
+                Tablero=CambiarTablero(Tablero,int(JugadaCPU),2)
+                PrintTablero(Tablero)
+                if(ComprobarEstado(Tablero) == 2):#Condicion Victoria del Server
+                    Tablero=[[0,0,0,0,0,0],[0,0,0,0,0,0],[0,0,0,0,0,0],[0,0,0,0,0,0],[0,0,0,0,0,0],[0,0,0,0,0,0]]
+                    msg="Gano CPU"
+                    client_socket.send(msg.encode())#Avisar al cliente de que gano el servidor
+                    conecta4_socket.sendto("Reset".encode(), (IP, serverPort))#Avisar al conecta4 de que se tiene que reiniciar
+                    print(">>>Gano la CPU<<<")
+                    print("==============================")
+
+                else:#Jugada normal donde nadie gano y se envia el jugada de cpu al cliente
+                    client_socket.send(JugadaCPU.encode())
+
 
